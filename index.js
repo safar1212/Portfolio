4d87cec4c5021b59d388a5d777c8bca731446154
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-02-28 21:30:46 +0300

    Mobile menu events added to take the user to page

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const burgerIcon1 = document.querySelector('#burgerIcon');
+const burgerIcon1 = document.querySelector("#burgerIcon");
 const crossIcon1 = document.querySelector("#crossIcon");
-burgerIcon1.addEventListener('click', () => {
-  document.getElementById('cover').style.cssText =
-    'width: 100%; height: 2000px;';
-  document.getElementById('crossIcon').style.cssText = "display:block; z-index: 2";
+const section = document.querySelectorAll("#n1");
+
+burgerIcon1.addEventListener("click", () => {
+  document.getElementById("cover").style.cssText =
+    "width: 100%; height: 2000px;";
+  document.getElementById("crossIcon").style.cssText =
+    "display:block; z-index: 2";
   document.getElementById("menu").style.cssText = "display: flex; z-index: 2;";
 });
-
-
-crossIcon1.addEventListener("click", function () {
+section.forEach((e) => {
+  e.addEventListener("click", function () {
     document.getElementById("cover").style.cssText = " width: 0%; height: 0%; ";
     document.getElementById("menu").style.cssText = "display: none;";
     document.getElementById("crossIcon").style.cssText = "display: none;";
   });
+});
+
+crossIcon1.addEventListener("click", function () {
+  document.getElementById("cover").style.cssText = " width: 0%; height: 0%; ";
+  document.getElementById("menu").style.cssText = "display: none;";
+  document.getElementById("crossIcon").style.cssText = "display: none;";
+});
