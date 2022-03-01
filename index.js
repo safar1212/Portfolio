beb6085ce803a85b794104ae7b027e8f08bc93a4
safar1212 <mm9240584@gmail.com>
2022-03-01 23:00:48 +0500

    created popup for the first button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// headline-section
+
 const burgerIcon1 = document.querySelector("#burgerIcon");
 const crossIcon1 = document.querySelector("#crossIcon");
 const section = document.querySelectorAll(".n4");
@@ -22,3 +24,68 @@ crossIcon1.addEventListener("click", function () {
   document.getElementsByClassName("menu").style.cssText = "display: none;";
   document.getElementById("crossIcon").style.cssText = "display: none;";
 });
+
+// projects-section
+
+
+const project = 
+  {
+    id: 'p1',
+    name: 'Tonic',
+    featuredImage: './images/picture-1.svg',
+    discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    technology: ['HTML', 'CSS', 'JavaScript'],
+    liveVersion: 'See Live',
+    source: 'See Source'
+  };
+
+   li = '';
+   project.technology.forEach ((project) => {
+     li +=`<li class="skills">${project}</li>`
+   });
+
+const popupCode = `
+<div class="page-2" id="proj">
+    
+<div class="pic">
+    <div class="heading">
+       <h2>${project.name}</h2>
+    </div>
+    <div class="year">
+       <p>Safar</p>
+       <p>Backend Dev</p>
+       <p>2015</p>
+    </div>
+    <div class="picture">
+        <a href="#"><img src= ${project.featuredImage} alt="picture-1"></a>
+    </div>
+</div>
+<div class="details">
+    <div class="para">
+        <p>${project.discription}</p>
+    </div>
+    <ul class="skills">
+        ${li}
+    </ul>   
+
+    <div class="buttons">
+        <button type="submit">${project.liveVersion}<img src= "./images/liveVersion.svg" alt="picture-1"></button>
+        <button type="submit">${project.source}<img src= "./images/source.svg" alt="picture-1"></button>
+    </div>
+</div>
+</div>
+`;
+
+popup.innerHTML = popupCode;
+
+const popup1 = document.querySelector('#popup1');
+popup1.addEventListener('click', () => {
+  console.log("asdfasdfasd");
+  document.getElementById('proj').style.cssText = 'z-index: 4;';
+  document.getElementById('popup').style.cssText = 'z-index: 4; display: block;';
+  document.getElementsByClassName('buttons').style.cssText ="display: flex;";
+  document.getElementsByClassName('skills').style.cssText ="border: red 2px solie;";
+});
+
+
+
